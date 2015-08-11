bb3b6c254306dbfdfdeb804fe2abaab12452410d
zhuufengpeixun <zhufengpeixun@126.com>
2015-08-11 15:32:05 +0800

    post

--- 6.server.js
+++ 6.server.js
@@ -7,13 +7,20 @@ http.createServer(function(request,response){
   var pathname = urls[0];
   var query = urls[1]?urls[1]:'';
   if(pathname == '/'){
-      fs.readFile('./form.html','utf8',function(err,data){
+      fs.readFile('./post.html','utf8',function(err,data){
           response.end(data);
       })
   }else if(pathname == '/addUser'){
-      var queryObj = util.parse(query);
-      users.push(queryObj);
-      response.end(JSON.stringify(users));
+      var msg = '';
+      request.on('data',function(data){
+          msg+=data.toString();
+      });
+      request.on('end',function(){
+          var userObj = util.parse(msg);
+          users.push(userObj);
+          response.end(JSON.stringify(users));
+      });
+
   }else{
       response.end('404');
   }
--- util/util.js
+++ util/util.js
@@ -2,7 +2,7 @@ function parse(query){
     var obj = {}, reg = /([^?=&]+)=([^?=&]+)/g;
     query.replace(reg, function () {
         var cur = arguments;
-        obj[cur[1]] = cur[2];
+        obj[cur[1]] = decodeURIComponent(cur[2]);
     });
     return obj;
 }

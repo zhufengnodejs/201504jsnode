a3423603faa768782658be1bc6db165ee3f39d40
zhuufengpeixun <zhufengpeixun@126.com>
2015-08-11 12:10:22 +0800

    init

--- 3.server.js
+++ 3.server.js
@@ -6,8 +6,20 @@ var server = http.createServer();
 // 请求 响应
 //服务员
 var wait = function(request,response){
+    // http://localhost:8080/rice
+    var path = request.url;
+    console.log(path);
     response.setHeader('Content-Type','text/html;charset=utf-8');
-    response.end(menu);//结束
+    if(path == '/rice'){
+        response.end('米饭');
+    }else if(path == '/fish'){
+        response.end('鱼香肉丝');
+    }else if(path == '/chicken'){
+        response.end('宫爆鸡丁');
+    }else{
+        response.end(menu);//结束
+    }
+
 }
 //当有客户端连接到来的时候，调用wait函数
 server.addListener('request',wait);
